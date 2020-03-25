689b127f99663fbf139edd1f0ca8a795e3fdb486
Ian Boldea <ianfboldea@gmail.com>
2020-03-25 19:18:21 -0400

    Added Register and Login Dummy Pages

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = ({ title, icon }) => {
   return (
-    <div className="navbar bg-primary">
+    <div className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
       <ul>
         <li>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/register'>Register</Link>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </div>
